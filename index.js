ce705009b6a97d9ec722741fd91204e2ef8b3bad
Carlos <miklo.concepcion@gmail.com>
2019-12-04 20:14:21 -0600

    built node api listening on port 127.0.0.1:3000. continuing on to crud operations

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// implement your API here
-// for the commit
+// implement your API here. building your API. node index.js to initiate api.
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.satusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World, from NodeJS");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`server listening on http://${hostname}:${port}`);
+});
