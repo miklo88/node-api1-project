61c7d94bfa72ec4afdcf4564df08305e9fe97e0b
Carlos <miklo.concepcion@gmail.com>
2019-12-12 14:47:12 -0600

    installed express and running on port 5000 from index.js file. going to build an API that performs CRUD operations on users. all dependencies are installed.

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 // implement your API here. building your API. node index.js to initiate api.
-const http = require("http");
+// const http = require("http");
+const express = require("express");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const port = 5000;
 
-const server = http.createServer((req, res) => {
-  res.satusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World, from NodeJS");
+const server = express();
+// const server = http.createServer((req, res) => {
+//   res.satusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World, from NodeJS");
+// });
+// everything form line 9 to 12 is below now. more dry.
+server.get("/", (req, res) => {
+  res.send("Hello world from express!");
+});
+// creating a route to the client side from the backend!
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`server listening on http://${hostname}:${port}`);
-});
+// // // // // // // // // // // // // // // // // // // // // // previous code - first iteration
+// // implement your API here. building your API. node index.js to initiate api.
+// const http = require("http");
+
+// const hostname = "127.0.0.1";
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//   res.satusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World, from NodeJS");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`server listening on http://${hostname}:${port}`);
+// });
