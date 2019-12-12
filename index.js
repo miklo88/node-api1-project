01a1ab285d62a7ac16366f0a64da51c2f47d7909
Carlos <miklo.concepcion@gmail.com>
2019-12-12 17:41:59 -0600

    CRUD operations created in projects with Hobbits created in index.js. API tested with insomnia! Moving onto next course segment to finish routing and pulling data from users.js and db.js. route established.

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
 // implement your API here. building your API. node index.js to initiate api.
 const express = require("express");
-
+const users = require("./data/seeds/users");
 const port = 5000;
 
 const server = express();
 
+// any requests for the http using the get method.
+// creating a route to the client side from the backend!
 server.get("/", (req, res) => {
   res.send("Lets get some LOTR characters!");
 });
-// creating a route to the client side from the backend!
+
+server.get("/hobbits", (req, res) => {
+  res.send("Welcome to Hobbiton");
+}); // READING DATA
+
+server.post("/hobbits", (req, res) => {
+  res.status(201).json({ url: "/hobbits", operation: "POST" });
+}); // CREATE DATA
+
+server.put("/hobbits", (req, res) => {
+  res.status(200).json({ url: "/hobbits", operation: "PUT" });
+}); // UPDATE DATA
+
+server.delete("/hobbits", (req, res) => {
+  res.sendStatus(204);
+}); // DELETE DATA
+
+server.get("/hobbits", (req, res) => {
+  const hobbits = [
+    {
+      id: 1,
+      name: "Samwise Gamgee"
+    },
+    {
+      id: 2,
+      name: "Frodo Baggins"
+    }
+  ];
+  res.status(200).json(hobbits);
+});
+
+server.get("/users", (req, res) => {
+  res.status(404).json(users);
+});
+
 server.listen(port, () => {
   console.log(`server listening on port ${port}`);
 });
